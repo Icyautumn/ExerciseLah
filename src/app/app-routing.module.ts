80cea7666b75cff1545c3bf2a0997767f86d2571
Icyautumn <victorchua865@gmail.com>
2022-06-29 15:45:45 +0800

    update fooddetails

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalorieTrackerComponent } from './calorie-tracker/calorie-tracker.component';
 import { CreatecontactComponent } from './createcontact/createcontact.component';
 import { DeletecontactComponent } from './deletecontact/deletecontact.component';
 import { DisplaycontactComponent } from './displaycontact/displaycontact.component';
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: 'updateContact', component: UpdatecontactComponent},
   { path: 'deleteContact', component: DeletecontactComponent},
   { path: 'workout', component: WorkoutComponent},
+  { path: 'calorie_tracker', component: CalorieTrackerComponent},
   { path: '', component: DisplaycontactComponent, pathMatch: 'full'}
 
 ];
---
+++ src/app/mock-foodDetails.ts
@@ -0,0 +1,119 @@
+import { FoodDetails } from "./foodDetails";
+
+export const listofFoodDetails: FoodDetails[] = [
+  {
+    _id: 1,
+    "items": [
+      {
+        "sugar_g": 1.2,
+        "fiber_g": 2.2,
+        "serving_size_g": 100,
+        "sodium_mg": 10,
+        "name": "potato",
+        "potassium_mg": 70,
+        "fat_saturated_g": 0,
+        "fat_total_g": 0.1,
+        "calories": 92.7,
+        "cholesterol_mg": 0,
+        "protein_g": 2.5,
+        "carbohydrates_total_g": 21
+      }
+    ]
+  },
+  {
+    _id: 2,
+    "items": [
+      {
+        "sugar_g": 12.3,
+        "fiber_g": 2.6,
+        "serving_size_g": 100,
+        "sodium_mg": 1,
+        "name": "banana",
+        "potassium_mg": 22,
+        "fat_saturated_g": 0.1,
+        "fat_total_g": 0.3,
+        "calories": 89.4,
+        "cholesterol_mg": 0,
+        "protein_g": 1.1,
+        "carbohydrates_total_g": 23.2
+      }
+    ]
+  },
+  {
+    _id: 3,
+    "items": [
+      {
+        "sugar_g": 5.7,
+        "fiber_g": 2.7,
+        "serving_size_g": 100,
+        "sodium_mg": 495,
+        "name": "bread",
+        "potassium_mg": 98,
+        "fat_saturated_g": 0.7,
+        "fat_total_g": 3.4,
+        "calories": 261.6,
+        "cholesterol_mg": 0,
+        "protein_g": 8.8,
+        "carbohydrates_total_g": 50.2
+      }
+    ]
+  },
+  {
+    _id: 4,
+    "items": [
+      {
+        "sugar_g": 1.1,
+        "fiber_g": 1.3,
+        "serving_size_g": 100,
+        "sodium_mg": 4,
+        "name": "spagetti",
+        "potassium_mg": 76,
+        "fat_saturated_g": 0.1,
+        "fat_total_g": 0.6,
+        "calories": 147.1,
+        "cholesterol_mg": 0,
+        "protein_g": 5.3,
+        "carbohydrates_total_g": 29.9
+      }
+    ]
+  },
+  {
+    _id: 5,
+    "items": [
+      {
+        "sugar_g": 0.6,
+        "fiber_g": 0.7,
+        "serving_size_g": 100,
+        "sodium_mg": 369,
+        "name": "chicken rice",
+        "potassium_mg": 36,
+        "fat_saturated_g": 0.7,
+        "fat_total_g": 3.4,
+        "calories": 134.9,
+        "cholesterol_mg": 0,
+        "protein_g": 2.6,
+        "carbohydrates_total_g": 23.6
+      }
+    ]
+  },
+  {
+    _id: 6,
+    "items": [
+      {
+        "sugar_g": 2.4,
+        "fiber_g": 0.7,
+        "serving_size_g": 100,
+        "sodium_mg": 414,
+        "name": "nasi lemak",
+        "potassium_mg": 102,
+        "fat_saturated_g": 4.6,
+        "fat_total_g": 10.9,
+        "calories": 191.3,
+        "cholesterol_mg": 41,
+        "protein_g": 6.9,
+        "carbohydrates_total_g": 17.4
+      }
+    ]
+  }
+
+]
